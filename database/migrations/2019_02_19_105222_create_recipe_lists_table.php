5380ad3da424819e8386ee694d6a07c70982333d
Christian Blom <christian.blom@chasacademy.se>
2019-02-19 20:06:33 +0100

    Started building the database, added basic JWT authentication.

---
+++ database/migrations/2019_02_19_105222_create_recipe_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipeListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe_lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->smallInteger('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe_lists');
+    }
+}
