a0d2f555bcf9658a33f68af4e5b94a6cd9334ca4
Christian Blom <christian.blom@chasacademy.se>
2019-02-20 22:43:21 +0100

    Added Recipe Controller, will start working on adding recipes to lists.

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Recipe;
+use App\RecipeList;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class RecipeController extends Controller
+{
+    public function addRecipeToList(Request $request)
+    {
+        # code...
+    }
+}
