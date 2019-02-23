10c97648a78a8bdd8c5c5f52f10596c158018726
Christian Blom <christian.blom@chasacademy.se>
2019-02-23 23:34:28 +0100

    Small tweak to api routes.

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,10 @@ Route::group(['middleware' => ['jwt.verify', 'cors']], function () {
     Route::get('closed', 'DataController@closed');
     
     Route::get('recipe_lists', 'RecipeListsController@getListsForUser');
+    Route::post('recipe_lists', 'RecipeListsController@createList');
+    
     Route::get('recipe_lists/{id}', 'RecipeListsController@getList');
     Route::put('recipe_lists/{id}', 'RecipeListsController@updateListName');
-    Route::post('recipe_lists', 'RecipeListsController@createList');
     Route::delete('recipe_lists/{id}', 'RecipeListsController@deleteList');
-
     Route::post('recipe_lists/{id}', 'RecipeController@addRecipeToList');
 });
